a602aa010822702fb09de0c23bd0b49665db7f2f
amadeuspzs <amadeus.stevenson@gmail.com>
2020-09-28 21:21:13 +0100

    Simplify

--- node-avr/node-avr.ino
+++ node-avr/node-avr.ino
@@ -1,51 +1,20 @@
 // **********************************************************************************
-// Based on Struct Send RFM69 Example
+// Based on Struct Send RFM69 Example: https://github.com/LowPowerLab/RFM69/blob/master/Examples/Struct_send/Struct_send.ino
 // **********************************************************************************
 // Copyright Felix Rusu 2018, http://www.LowPowerLab.com/contact
 // **********************************************************************************
-// License
-// **********************************************************************************
-// This program is free software; you can redistribute it 
-// and/or modify it under the terms of the GNU General    
-// Public License as published by the Free Software       
-// Foundation; either version 3 of the License, or        
-// (at your option) any later version.                    
-//                                                        
-// This program is distributed in the hope that it will   
-// be useful, but WITHOUT ANY WARRANTY; without even the  
-// implied warranty of MERCHANTABILITY or FITNESS FOR A   
-// PARTICULAR PURPOSE. See the GNU General Public        
-// License for more details.                              
-//                                                        
-// Licence can be viewed at                               
-// http://www.gnu.org/licenses/gpl-3.0.txt
-//
-// Please maintain this license information along with authorship
-// and copyright notices in any redistribution of this code
-// **********************************************************************************
+
 #include <RFM69.h>         //get it here: https://github.com/amadeuspzs/RFM69
 #include <RFM69_ATC.h>     //get it here: https://github.com/amadeuspzs/RFM69
 #include <SPI.h>           //included with Arduino IDE install (www.arduino.cc)
-//*********************************************************************************************
-//************ IMPORTANT SETTINGS - YOU MUST CHANGE/CONFIGURE TO FIT YOUR HARDWARE *************
-//*********************************************************************************************
+
 #define NODEID      2
 #define NETWORKID   1
 #define GATEWAYID   1
-//Match frequency to the hardware version of the radio on your Moteino (uncomment one):
 #define FREQUENCY     RF69_433MHZ
-//#define FREQUENCY     RF69_868MHZ
-//#define FREQUENCY     RF69_915MHZ
-#define ENCRYPTKEY    "sampleEncryptKey" //has to be same 16 characters/bytes on all nodes, not more not less!
+//#define ENCRYPTKEY    "sampleEncryptKey" //has to be same 16 characters/bytes on all nodes, not more not less!
 //#define IS_RFM69HW_HCW  //uncomment only for RFM69HW/HCW! Leave out if you have RFM69W/CW!
-//*********************************************************************************************
-//Auto Transmission Control - dials down transmit power to save battery
-//Usually you do not need to always transmit at max output power
-//By reducing TX power even a little you save a significant amount of battery power
-//This setting enables this gateway to work with remote nodes that have ATC enabled to
-//dial their power down to only the required level
 #define ENABLE_ATC    //comment out this line to disable AUTO TRANSMISSION CONTROL
-//*********************************************************************************************
 #define SERIAL_BAUD 115200
 
 #ifdef ENABLE_ATC
@@ -59,9 +28,7 @@ byte sendSize=0;
 boolean requestACK = true;
 
 typedef struct {
-  //int           nodeId; //store this nodeId
-  unsigned long uptime; //uptime in ms
-  float         temp;   //temperature maybe?
+  bool state = false;
 } Payload;
 Payload theData;
 
@@ -71,18 +38,14 @@ void setup() {
 #ifdef IS_RFM69HW_HCW
   radio.setHighPower(); //must include this only for RFM69HW/HCW!
 #endif
-  radio.encrypt(ENCRYPTKEY);
+  //radio.encrypt(ENCRYPTKEY);
   Serial.print("Transmitting at ");
   Serial.print(FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
   Serial.println(" MHz");
 }
 
-void loop() {
-  //fill in the struct with new values
-  //theData.nodeId = NODEID;
-  theData.uptime = millis();
-  theData.temp = 99.99; //it's hot!
-  
+void loop() {  
+  theData.state = true;
   Serial.print("Sending struct (");
   Serial.print(sizeof(theData));
   Serial.print(" bytes) ... ");
