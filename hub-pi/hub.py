83324ed7d67e6aed91f393dd1e288588c8e213fd
Amadeus Stevenson <amadeus.stevenson@gmail.com>
2020-09-28 21:19:46 +0100

    Working mqtt

--- hub-pi/hub.py
+++ hub-pi/hub.py
@@ -1,9 +1,12 @@
 from RFM69 import Radio, FREQ_433MHZ
+import paho.mqtt.publish as publish
 import datetime
 import time
 
 node_id = 1
 network_id = 1
+topic="bantam/door"
+
 #encryptKey = "sampleEncryptKey"
 
 #with Radio(FREQ_433MHZ, node_id, network_id, isHighPower=False, verbose=True, encryptionKey=encryptKey) as radio:
@@ -11,5 +14,12 @@ with Radio(FREQ_433MHZ, node_id, network_id, isHighPower=False, verbose=False) a
     while True:
         # Process packets
         for packet in radio.get_packets():
-            print (packet)
+	    if (packet.data[0] == 0):
+		payload="Closed"
+	    else:
+		payload="Open"
+            print(packet)
+	    print(packet.data)
+	    print(payload)
+	    publish.single(topic, payload=payload, qos=0, retain=False, hostname="192.168.1.199", port=1883, client_id="rfm69-pi", keepalive=60, will=None, auth=None, tls=None, transport="tcp")
         time.sleep(1)
