be2d0a8b9d8b5fa8ff7db0cc7d75930fd1151869
Amadeus Stevenson <amadeus.stevenson@gmail.com>
2020-11-16 09:47:35 +0000

    Debugging code

--- hub-pi/hub.py
+++ hub-pi/hub.py
@@ -1,3 +1,4 @@
+print("Starting up")
 from RFM69 import Radio, FREQ_433MHZ
 import paho.mqtt.publish as publish
 import time
@@ -7,15 +8,17 @@ network_id = 1
 #encryptKey = "sampleEncryptKey"
 
 def processPacket(packet):
+	print("Processing data")
 	if packet.sender == 2:
 		topic = "bantam/door"
-		payload = "Open" if packet.data == 0 else "Closed"
+		payload = "Open" if packet.data == 0 else "Closed" if packet.data == 1 else "Undefined"
 
 	return({ "topic": topic, "payload": payload})
 
 #with Radio(FREQ_433MHZ, node_id, network_id, isHighPower=False, verbose=True, encryptionKey=encryptKey) as radio:
 with Radio(FREQ_433MHZ, node_id, network_id, isHighPower=False, verbose=False) as radio:
 	while True:
+		print("Waiting for packets")
 		# Process packets
 		for packet in radio.get_packets():
 			data = processPacket(packet)
