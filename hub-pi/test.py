4c021b22c5192d40dca2830402c38d815de8c91d
Amadeus Stevenson <amadeus.stevenson@gmail.com>
2020-11-16 10:11:32 +0000

    Add test receive code

---
+++ hub-pi/test.py
@@ -0,0 +1,26 @@
+from RFM69 import Radio, FREQ_433MHZ
+import datetime
+import time
+
+node_id = 1
+network_id = 1
+
+with Radio(FREQ_433MHZ, node_id, network_id, isHighPower=False, verbose=True) as radio:
+    print ("Starting loop...")
+    
+    rx_counter = 0
+
+    while True:
+        
+        # Every 10 seconds get packets
+        if rx_counter > 10:
+            rx_counter = 0
+            
+            # Process packets
+            for packet in radio.get_packets():
+                print (packet)
+
+        print("Listening...", len(radio.packets), radio.mode_name)
+        delay = 0.5
+        rx_counter += delay
+        time.sleep(delay)
