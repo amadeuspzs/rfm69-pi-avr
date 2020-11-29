f3627d54bb3d018d87c3d58a3d863bd8dd865d6d
amadeuspzs <amadeus.stevenson@gmail.com>
2020-11-29 21:37:37 +0000

    Working code

--- node-avr/node-avr.ino
+++ node-avr/node-avr.ino
@@ -1,9 +1,7 @@
-// **********************************************************************************
-// Based on Struct Send RFM69 Example: https://github.com/LowPowerLab/RFM69/blob/master/Examples/Struct_send/Struct_send.ino
-// **********************************************************************************
-// Copyright Felix Rusu 2018, http://www.LowPowerLab.com/contact
-// **********************************************************************************
+#define debug true // true for serial output
 
+#include <avr/sleep.h>
+#include "PinChangeInterrupt.h" // for PCINTX
 #include <RFM69.h>         //get it here: https://github.com/amadeuspzs/RFM69
 #include <RFM69_ATC.h>     //get it here: https://github.com/amadeuspzs/RFM69
 #include <SPI.h>           //included with Arduino IDE install (www.arduino.cc)
@@ -15,7 +13,8 @@
 //#define ENCRYPTKEY    "sampleEncryptKey" //has to be same 16 characters/bytes on all nodes, not more not less!
 //#define IS_RFM69HW_HCW  //uncomment only for RFM69HW/HCW! Leave out if you have RFM69W/CW!
 #define ENABLE_ATC    //comment out this line to disable AUTO TRANSMISSION CONTROL
-#define SERIAL_BAUD 115200
+#define ATC_RSSI      -80 // target RSSI (dBm)  
+#define SERIAL_BAUD 9600
 
 #ifdef ENABLE_ATC
   RFM69_ATC radio;
@@ -23,40 +22,76 @@
   RFM69 radio;
 #endif
 
-int TRANSMITPERIOD = 300; //transmit a packet to gateway so often (in ms)
-byte sendSize=0;
-boolean requestACK = true;
+int sensor = 7; // Reed switch attached to D7=PIN6=PA7=PCINT7
+
+int numRetries = 3; // default is 2
+int timeout = 255; // ms to wait for ACK, default is 30
 
 typedef struct {
-  bool state = false;
+  bool state;
 } Payload;
 Payload theData;
 
-int sensor = 5;
-
 void setup() {
-  Serial.begin(SERIAL_BAUD);
+  pinMode(sensor,INPUT_PULLUP);
+  delay(2000); // allow debounce cap to charge
+
+  if (debug) {
+    Serial.begin(SERIAL_BAUD);
+    while (!Serial) ;
+  }
+  
   radio.initialize(FREQUENCY,NODEID,NETWORKID);
-#ifdef IS_RFM69HW_HCW
-  radio.setHighPower(); //must include this only for RFM69HW/HCW!
-#endif
+  #ifdef IS_RFM69HW_HCW
+    radio.setHighPower(); //must include this only for RFM69HW/HCW!
+  #endif
+  #ifdef ENABLE_ATC
+    radio.enableAutoPower(ATC_RSSI); // set the target RSSI
+  #endif
+
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  
   //radio.encrypt(ENCRYPTKEY);
-  Serial.print("Transmitting at ");
-  Serial.print(FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
-  Serial.println(" MHz");
-  pinMode(sensor,INPUT);
+  if (debug) {
+    Serial.print("Transmitting at ");
+    Serial.print(FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
+    Serial.println(" MHz");
+  }
 }
 
-void loop() {  
-  theData.state = digitalRead(sensor) == HIGH;
-  Serial.print("State: ");
-  Serial.println(theData.state);
-  Serial.print("Sending struct (");
-  Serial.print(sizeof(theData));
-  Serial.print(" bytes) ... ");
-  if (radio.sendWithRetry(GATEWAYID, (const void*)(&theData), sizeof(theData)))
-    Serial.print(" ok!");
-  else Serial.print(" nothing...");
-  Serial.println();
-  delay(10000); // delay 10 seconds
+void loop() {
+  
+  theData.state = digitalRead(sensor);
+
+  if (debug) {
+    Serial.print("Sending struct (");
+    Serial.print(sizeof(theData));
+    Serial.print(" bytes): ");
+    Serial.print(theData.state);
+    Serial.print(" ... ");
+  }
+  
+  if (radio.sendWithRetry(GATEWAYID, (const void*)(&theData), sizeof(theData), numRetries, timeout)) {
+    if (debug) Serial.print(" ok!");
+  } else { 
+    if (debug) Serial.print(" nothing...");
+  }
+
+  if (debug) {
+    Serial.println();
+    Serial.println("Dropping into sleep");
+    Serial.flush();
+  }
+  
+  attachPCINT(digitalPinToPCINT(sensor), wake, CHANGE);
+
+  sleep_enable(); // enable sleep
+  sleep_cpu();   // go to sleep
 }
+
+void wake()
+{
+  detachPCINT(digitalPinToPCINT(sensor));
+  sleep_disable();
+  // this will then go back into the loop() function
+} //end of wake
